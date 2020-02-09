248be9b8df8a6027eb5b2ab7cc9f1c98b963be43
ZXCandice <60077387+ZXCandice@users.noreply.github.com>
2020-02-09 13:44:48 +0000

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,6 +1,5 @@
 from adapt.intent import IntentBuilder
 from mycroft import MycroftSkill, intent_file_handler
-from messagebus.message import message
 
 class Test(MycroftSkill):
     def __init__(self):
